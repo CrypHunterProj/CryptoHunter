69013afd04338521d0ec571acca6a4332354e08e
Albert <albert@MacBook-Pro-de-Albert.local>
2021-05-13 11:24:34 -0300

    Adding currency cell and mocked model

---
+++ CryptoHunter/View/DashboardHeader.swift
@@ -0,0 +1,32 @@
+//
+//  DashboardHeader.swift
+//  CryptoHunter
+//
+//  Created by Albert on 13/05/21.
+//
+// Charts Resources: https://github.com/AppPear/ChartView
+
+import SwiftUI
+import SwiftUICharts
+
+struct DashboardHeader: View {
+    var body: some View {
+        HStack {
+            LineChartView(data: [1,12,4,15],
+                          title: "BTC",
+                          form: ChartForm.small)
+            LineChartView(data: [20,12,44,15],
+                          title: "ETH",
+                          form: ChartForm.small)
+            LineChartView(data: [12,12,4,15],
+                          title: "ADV",
+                          form: ChartForm.small)
+        }
+    }
+}
+
+struct DashboardHeader_Previews: PreviewProvider {
+    static var previews: some View {
+        DashboardHeader()
+    }
+}
