6fa01ceec29faeb2215753b4be46fa34c31950b3
All3yp <noyalle@ualberta.ca>
2021-05-18 19:35:11 -0300

    CurrencyServices complete: Use func gettingCryptocoins to read cryptocoins from request or persistence

--- CryptoHunter/Service/CurrencyService.swift
+++ CryptoHunter/Service/CurrencyService.swift
@@ -34,8 +34,47 @@ struct CoinMarketCapRouter {
 
 struct CurrencyService {
 
-    static func request(session: URLSession = URLSession.shared,
-                        completion: @escaping (Result<[CryptoCoin],Error>) -> Void) {
+    /// Função que abstrai a lógica que lê e retorna o array de CryptoCoin. Caso já tenha feito a requisição do dia atual, pega da persistencia. Se não, faz a requisição, persiste o resultado e retorna os dados baixados.
+    /// - Parameters:
+    ///     - completion: Retorna o array de CryptoCoin em uma closure
+    ///     - cryptoCoinArray: Array contendo as informações das criptomoedas
+    static func gettingCryptocoins(completion: @escaping (_ cryptoCoinArray: [CryptoCoin]) -> Void) {
+        if hasPersistedCryptocoinsForToday() {
+            let readDocumentsURL = FileManager.default.urls(for: .documentDirectory, in: .allDomainsMask).first!
+            let readFileURL = readDocumentsURL.appendingPathComponent(gettingListingFilename())
+            do {
+                let data = try Data(contentsOf: readFileURL)
+                let cryptoData = try JSONDecoder().decode(CryptoData.self, from: data)
+                completion(cryptoData.data)
+            } catch {
+                print(error)
+                completion([])
+            }
+        } else {
+            request { result in
+                switch result {
+                case .success(let cryptocoin):
+                    completion(cryptocoin)
+                case .failure(let error):
+                    print(error)
+                    completion([])
+                }
+            }
+        }
+    }
+
+    /// Verifica se há um arquivo persistido no dia e retorna um booleano
+    private static func hasPersistedCryptocoinsForToday() -> Bool {
+        //Gerando caminho para o arquivo do dia atual
+        let documentsURL: URL = FileManager.default.urls(for: .documentDirectory, in: .allDomainsMask).first!
+        let fileURL: URL = documentsURL.appendingPathComponent(gettingListingFilename())
+        let filePath: String = fileURL.relativePath
+
+        return FileManager.default.fileExists(atPath: filePath)
+    }
+
+    private static func request(session: URLSession = URLSession.shared,
+                                completion: @escaping (Result<[CryptoCoin],Error>) -> Void) {
 
         let task = session.dataTask(with: CoinMarketCapRouter.urlRequest) { data, response, error in
             guard let data = data, error == nil else {
@@ -46,11 +85,38 @@ struct CurrencyService {
             do {
                 let cryptoData = try JSONDecoder().decode(CryptoData.self, from: data)
                 completion(.success(cryptoData.data))
+                try persistListingsCurrency(data: data)
             }
             catch {
                 completion(.failure(CurrencyServiceError.decodeError(errorDescription: "\(error)")))
             }
         }
         task.resume()
+    }
+
+
+    /// Salva no dispositivo o `data` do `CryptoData` para o dia atual. Usada dentro da função `request`.
+    /// - Parameter data: Data do json que foi retornado na requisicao.
+    /// - Throws: Erro que o data.write lança.
+    private static func persistListingsCurrency(data: Data) throws {
+        //Gerando caminho para o arquivo do dia atual
+        let documentsURL: URL = FileManager.default.urls(for: .documentDirectory, in: .allDomainsMask).first!
+        let fileURL: URL = documentsURL.appendingPathComponent(gettingListingFilename())
+
+        //Persistindo data no caminho
+        try data.write(to: fileURL)
+    }
+
+
+    /// Função que retorna o titulo padronizado pra que o nome do arquivo persistido seja unico por dia e identifique o dia atual. Dessa forma, todos os dias, o titulo do arquivo do dia atual é diferente do dia anterior.
+    /// - Returns: Titulo do arquivo para o dia atual diferente do dia anterior.
+    ///
+    /// Exemplo de retorno:
+    ///     "19-04-2021_ListingsCryptocurrency.json"
+    ///
+    private static func gettingListingFilename() -> String {
+        let dateComponents = Calendar.current.dateComponents([ .day, .month, .year ], from: Date())
+        let dateListeningCurrency: String = "\(dateComponents.day!)-\(dateComponents.month!)-\(dateComponents.year!)_ListingsCryptocurrency.json"
+        return dateListeningCurrency
     }
 }
--- CryptoHunter/View/InformationView/Information.swift
+++ CryptoHunter/View/InformationView/Information.swift
@@ -33,81 +33,15 @@ struct Information: View {
                     }
                     .foregroundColor(.white)
                 }
-            /*  DEBUG
                 .onAppear(perform: {
-                    let fetchInfo = FetchInformation()
-                    let information = fetchInfo.parseJson(filename: "Informacoes")
-                    print(information)
+// MARK: - Debug
+                    /*
+                     CurrencyService.gettingCryptocoins { cryptoCoins in
+                     print(cryptoCoins)
+                     }
+                     */
                 })
-            */
-                //  DEBUG
-                    .onAppear(perform: {
-
-//                        CurrencyService.request() { result in
-//                            switch result {
-//                            case .success(let result):
-//                                print(result)
-//                            case .failure(let error):
-//                                print(error)
-//                            }
-//                        }
-
-//                        do {
-//
-//                            let blz = try JSONDecoder().decode(
-//                                CryptoData.self,
-//                                from:
-//                                    try Data(
-//                                        contentsOf:
-//                                            Bundle.main.url(
-//                                                forResource: "Coinmarketcap",
-//                                                withExtension: "json"
-//                                            )!
-//                                    )
-//                            )
-//
-//                            let rankblz = blz.data.filter { $0.cmc_rank <= 100 }.sorted { $0.cmc_rank < $1.cmc_rank }
-//
-//                            print("\n\n👺👺👺👺👺👺👺👺👺👺👺👺👺👺👺👺👺👺\n\n")
-//                            for b in rankblz {
-//                                print("\(b.name) \(b.quote.BRL!.price)")
-//                            }
-//
-//                            print("\n\n🥺🥺🥺🥺🥺🥺🥺🥺")
-//                            print("total -", rankblz.count)
-//                            print("1h -", rankblz.filter { c in return c.quote.BRL!.percent_change_1h != 0 }.count)
-//                            print("24h -", rankblz.filter { c in return c.quote.BRL!.percent_change_24h != 0 }.count)
-//                            print("7d -", rankblz.filter { c in return c.quote.BRL!.percent_change_7d != 0 }.count)
-//                            print("30d -", rankblz.filter { c in return c.quote.BRL!.percent_change_30d != 0 }.count)
-//                            print("60d - ", rankblz.filter { c in return c.quote.BRL!.percent_change_60d != 0 }.count)
-//                            print("90d -", rankblz.filter { c in return c.quote.BRL!.percent_change_90d != 0 }.count)
-//                            print("market_cap -", rankblz.filter { c in return c.quote.BRL!.market_cap != 0 }.count)
-//                            print("price -", rankblz.filter { c in return c.quote.BRL!.price != 0 }.count)
-//
-//                            print("\n\n🧐")
-//
-//                            print("30d -", rankblz.filter { c in return c.quote.BRL!.percent_change_30d != 0 }.map { $0.name })
-//                            print("60d - ", rankblz.filter { c in return c.quote.BRL!.percent_change_60d != 0 }.map { $0.name })
-//                            print("90d -", rankblz.filter { c in return c.quote.BRL!.percent_change_90d != 0 }.map { $0.name })
-//
-//
-//                        } catch {
-//                            print(error)
-//                        }
-
-//                        CurrentService.shared.getAllCryptoData {  result in
-//                            switch result {
-//                            case .success(let sortedCryptos):
-//                                print("\n|-----------------------------------|")
-//                                print("\nINFORMATION CRYPTOS\n")
-//                                print(sortedCryptos.first)
-//                            case .failure(let error):
-//                                print(error)
-//                            }
-//                        }
-                    })
             }
-
             .navigationTitle("Informações")
         }
     }
