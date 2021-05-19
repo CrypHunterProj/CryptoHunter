ce81a765414ff47c386f89f93edaf13bc9f8dd92
All3yp <noyalle@ualberta.ca>
2021-05-19 11:19:55 -0300

    BuxFix button navigationItem name, Separate archives structs CurrentService and CoinmasterCapRouter, showing in OnAppear results API named, symbol, and quotes.

--- CryptoHunter/Service/CurrencyService.swift
+++ CryptoHunter/Service/CurrencyService.swift
@@ -7,31 +7,6 @@
 
 import Foundation
 
-struct CoinMarketCapRouter {
-
-    private init() { }
-
-    static var urlRequest: URLRequest {
-        var request = URLRequest(url: url)
-        request.allHTTPHeaderFields = header
-        return request
-    }
-
-    private static let url: URL = URL(
-        string: "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=100&convert=BRL"
-    )!
-
-    private static var header: [String: String] {
-        switch self {
-        default:
-            return [
-                "Accepts": "application/json",
-                "X-CMC_PRO_API_KEY": "89fe9de0-0581-4265-812e-84c7e2b2f59a"
-            ]
-        }
-    }
-}
-
 struct CurrencyService {
 
     /// Função que abstrai a lógica que lê e retorna o array de CryptoCoin. Caso já tenha feito a requisição do dia atual, pega da persistencia. Se não, faz a requisição, persiste o resultado e retorna os dados baixados.
