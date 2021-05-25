33b8249e9113f52ef6651dc392d265e5a4f6269a
CrisCoutinho <50340670+CrisCoutinho@users.noreply.github.com>
2021-05-25 10:55:18 -0300

    Create func previewChartStyle

--- CryptoHunter/View/Dashboard/DashboardHeader.swift
+++ CryptoHunter/View/Dashboard/DashboardHeader.swift
@@ -14,6 +14,7 @@ struct DashboardHeader: View {
         HStack {
             LineChartView(data: [1, 12, 4, 15],
                           title: "BTC",
+                          style: previewChartStyle(up: true),
                           form: CGSize(width: 120, height: 100),
                           dropShadow: false)
             LineChartView(data: [20, 12, 44, 15],
@@ -25,6 +26,31 @@ struct DashboardHeader: View {
                           form: CGSize(width: 120, height: 100),
                           dropShadow: false)
         }.padding(.vertical, 16)
+    }
+
+    func previewChartStyle(up: Bool) -> ChartStyle {
+        let chartStyle: ChartStyle
+        let darkMode: ChartStyle
+
+        let gradient = GradientColor(start: up ? Color("crescentGraphStart") :
+                                        Color("descendingGraphStart"),
+                                     end: up ? Color("crescentGraphEnd"): Color("descendingGraphEnd") )
+
+        chartStyle = ChartStyle(backgroundColor: Color("backgroundGraphStart"),
+                                    accentColor: .pink,
+                                    gradientColor: GradientColor(start: up ? .green :
+                                                                    .red,
+                                        end: up ?
+                                            .gray: .pink),
+                                    textColor: Color("textTabItemUnselected"),
+                                    legendTextColor: .pink,
+                                    dropShadowColor: .clear)
+        darkMode = chartStyle
+
+        chartStyle.darkModeStyle = darkMode
+
+        return chartStyle
+
     }
 }
 
